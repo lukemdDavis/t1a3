1c72c61c45174c4c2883703c84b724dc2f4c7cd0
LukemdDavis <davisluke656@gmail.com>
2020-12-19 12:24:32 +1000

    made the options list a method called mainmenu

--- shopping_list.rb
+++ shopping_list.rb
@@ -4,16 +4,18 @@ puts "what would you like to call you shopping list?"
 list_name = gets.chomp
 puts "Alright #{name} lets start adding to #{list_name}"
 
-
-     puts   "1. View shopping list",
+def maiMenu
+        "1. View shopping list",
         "2. Add item to shopping list",
         "3. Remove item from shopping list",
         "4. Check if item is on shopping list",
         "5. How many items on shopping list",
         "6. Clear shopping list",
         "7. Exit"
-
+return option
 
 
 
 shopping_list = ["apples", "lettuce", "tomatoes", "potatoes", "steak", "milk", "cheese", "chips"]
+
+
