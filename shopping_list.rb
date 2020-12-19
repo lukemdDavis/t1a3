ac3cecafc093187251327cb994a66ec1d6ed8850
LukemdDavis <davisluke656@gmail.com>
2020-12-19 12:14:56 +1000

    made array called shopping_list

--- shopping_list.rb
+++ shopping_list.rb
@@ -3,3 +3,6 @@ name = gets.chomp
 puts "what would you like to call you shopping list?"
 list_name = gets.chomp
 puts "Alright #{name} lets start adding to #{list_name}"
+
+
+shopping_list = ["apples", "lettuce", "tomatoes", "potatoes", "steak", "milk", "cheese", "chips"]
