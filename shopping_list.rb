fa41aa2dca8101781445399d4df7ff6bfdd3f4c1
LukemdDavis <davisluke656@gmail.com>
2020-12-19 12:52:30 +1000

    made the list_name a global variable

--- shopping_list.rb
+++ shopping_list.rb
@@ -2,7 +2,7 @@ puts "what is your name"
 name = gets.chomp
 puts "what would you like to call you shopping list?"
 list_name = gets.chomp
-puts "Alright #{name} lets start adding to #{list_name}"
+puts "Alright #{name} lets start adding to #{$list_name}"
 
 def maiMenu
         "1. View shopping list",
