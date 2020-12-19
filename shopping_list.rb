232c8145d94f6c2190bafeebf369fd0ae59f5b13
LukemdDavis <davisluke656@gmail.com>
2020-12-19 12:18:56 +1000

    made the main menu list

--- shopping_list.rb
+++ shopping_list.rb
@@ -5,4 +5,15 @@ list_name = gets.chomp
 puts "Alright #{name} lets start adding to #{list_name}"
 
 
+     puts   "1. View shopping list",
+        "2. Add item to shopping list",
+        "3. Remove item from shopping list",
+        "4. Check if item is on shopping list",
+        "5. How many items on shopping list",
+        "6. Clear shopping list",
+        "7. Exit"
+
+
+
+
 shopping_list = ["apples", "lettuce", "tomatoes", "potatoes", "steak", "milk", "cheese", "chips"]
